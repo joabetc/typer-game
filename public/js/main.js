e76c79e248b68e55d934c444dbd0c7a8418ede81
Joabe Costa <joabetc@hotmail.com>
2018-01-31 01:23:23 -0200

    Counting words and characters while clicking

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,13 @@ var phrase = $(".phrase").text();
 var wordNumber = phrase.split(" ").length;
 var phraseSize = $("#phrase-size");
 phraseSize.text(wordNumber);
+
+var typingField = $(".typing-field");
+typingField.on("click", function() {
+    var content = typingField.val();
+    var numberWords = content.split(" ").length;
+    $("#word-counter").text(numberWords);
+
+    var numberChars = content.length;
+    $("#char-counter").text(numberChars);
+});
