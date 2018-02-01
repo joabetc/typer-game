2350317a3ee28de7f2bda9994d679ead00f26fd1
Joabe Costa <joabetc@hotmail.com>
2018-02-01 20:12:57 -0200

    Segregating code in files

---
+++ public/js/score.js
@@ -0,0 +1,29 @@
+function registerScore() {
+    var tableBody = $(".score").find("tbody");
+    var user = "Me";
+    var numWords = $("#word-counter").text();
+    var tableRow = createRow(user, numWords);
+    tableRow.find(".remove-button").click(removeRow);
+    tableBody.prepend(tableRow);
+}
+
+function createRow(user, numWords) {
+    var tableRow = $("<tr>");
+    var userColumn = $("<td>").text(user);
+    var wordsColumn = $("<td>").text(numWords);
+    var buttonColumn = $("<td>");
+    var link = $("<a>").addClass("remove-button").attr("href", "#");
+    var icon = $("<i>").addClass("small").addClass("material-icons").text("delete");
+    link.append(icon);
+    buttonColumn.append(link);
+    tableRow.append(userColumn);
+    tableRow.append(wordsColumn);
+    tableRow.append(buttonColumn);
+
+    return tableRow;
+}
+
+function removeRow(event) {
+    event.preventDefault();
+    $(this).parent().parent().remove();
+}
