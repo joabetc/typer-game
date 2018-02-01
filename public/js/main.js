d4a608d2308a168e1bb435f35c9f34a53fd04225
Joabe Costa <joabetc@hotmail.com>
2018-02-01 20:08:47 -0200

    Creating element through jQuery

--- public/js/main.js
+++ public/js/main.js
@@ -64,18 +64,31 @@ function registerScore() {
     var tableBody = $(".score").find("tbody");
     var user = "Me";
     var numWords = $("#word-counter").text();
-    var removeButton = "<a href='#' class='remove-button'><i class='small material-icons'>delete</i></a>";
-    var tableRow = "<tr><td>" + user + 
-                   "</td><td>" + numWords + 
-                   "</td><td>" + removeButton +
-                   "</td></tr>";
+    var tableRow = createRow(user, numWords);
+    tableRow.find(".remove-button").click(removeRow);
     tableBody.prepend(tableRow);
 }
 
-$("remove-button").click(function(event) {
+function createRow(user, numWords) {
+    var tableRow = $("<tr>");
+    var userColumn = $("<td>").text(user);
+    var wordsColumn = $("<td>").text(numWords);
+    var buttonColumn = $("<td>");
+    var link = $("<a>").addClass("remove-button").attr("href", "#");
+    var icon = $("<i>").addClass("small").addClass("material-icons").text("delete");
+    link.append(icon);
+    buttonColumn.append(link);
+    tableRow.append(userColumn);
+    tableRow.append(wordsColumn);
+    tableRow.append(buttonColumn);
+
+    return tableRow;
+}
+
+function removeRow(event) {
     event.preventDefault();
-    this.parent().parent().remove();
-});
+    $(this).parent().parent().remove();
+}
 
 function restarGame() {
     typingField.attr("disabled", false);
