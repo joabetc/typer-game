d03f83cb08194573571a0207c3c91896e3f6509c
Joabe Costa <joabetc@hotmail.com>
2018-02-13 20:48:54 -0200

    Getting a phrase through a random position

--- public/js/phrase.js
+++ public/js/phrase.js
@@ -1,8 +1,11 @@
 $("#phrase-button").click(randomPhrase);
 
 function randomPhrase() {
-    $.get("http://localhost:3000/frases", function(data) {
-        var phrase = $(".phrase");
-        phrase.text(data[0].texto);
-    });
+    $.get("http://localhost:3000/frases", changePhrase);
+}
+
+function changePhrase(data) {
+    var phrase = $(".phrase");
+    var randomNumber = Math.floor(Math.random() * data.length);
+    phrase.text(data[randomNumber].texto);
 }
