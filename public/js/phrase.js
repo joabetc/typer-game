8e12f924b93bb41a3a0b360f588a3e15cedd2603
Joabe Costa <joabetc@hotmail.com>
2018-02-13 20:42:51 -0200

    Using Ajax to get the first phrase from server

--- public/js/phrase.js
+++ public/js/phrase.js
@@ -2,6 +2,7 @@ $("#phrase-button").click(randomPhrase);
 
 function randomPhrase() {
     $.get("http://localhost:3000/frases", function(data) {
-        console.log(data);
+        var phrase = $(".phrase");
+        phrase.text(data[0].texto);
     });
 }
