940404ee990746033621940eb63c662d6ad48198
Joabe Costa <joabetc@hotmail.com>
2018-02-13 20:40:18 -0200

    Ajax with get

--- public/js/phrase.js
+++ public/js/phrase.js
@@ -1,5 +1,7 @@
 $("#phrase-button").click(randomPhrase);
 
 function randomPhrase() {
-    console.log("Click");
+    $.get("http://localhost:3000/frases", function(data) {
+        console.log(data);
+    });
 }
