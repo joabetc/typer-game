7c5d7ad4e40b9b2cf9142623bec0d6179d2c9127
Joabe Costa <joabetc@hotmail.com>
2018-02-14 11:58:26 -0200

    Using AJAX get with an id to return specific id

--- public/js/phrase.js
+++ public/js/phrase.js
@@ -1,4 +1,5 @@
 $("#phrase-button").click(randomPhrase);
+$("#phrase-id-button").click(findPhraseById);
 
 function randomPhrase() {
     $("#spinner").show();
@@ -20,4 +21,27 @@ function changePhrase(data) {
     phrase.text(data[randomNumber].texto);
     updatePhraseSize();
     updateStartTypingTime(data[randomNumber].tempo);
+}
+
+function findPhraseById() {
+    $("#spinner").show();
+    var phraseId = $("#phrase-id").val();
+    var phraseData = { id: phraseId };
+    $.get("http://localhost:3000/frases", phraseData, changePhraseById)
+        .fail(function() {
+            $("#error").show();
+            setTimeout(function() {
+                $("#error").toggle();
+            }, 2000);
+        })
+        .always(function() {
+            $("#spinner").toggle();
+        });
+}
+
+function changePhraseById(data) {
+    var phrase = $(".phrase");
+    phrase.text(data.texto);
+    updatePhraseSize();
+    updateStartTypingTime(data.tempo);
 }
